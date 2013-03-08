7a0cbcbe65efc89990806460df43178856347a38
zhuc <zc@gokuai.com>
2013-03-08 09:38:03 +0800

    删除注释

--- start/common/js/login.js
+++ start/common/js/login.js
@@ -485,20 +485,15 @@ var gkClientLogin = {
         };
         //处理键盘事件 禁止后退键（Backspace）密码或单行、多行文本框除外  
         function banBackSpace(e){     
-            console.log(1);
             var ev = e || window.event;//获取event对象     
             var obj = ev.target || ev.srcElement;//获取事件源     
-      
             var t = obj.type || obj.getAttribute('type');//获取事件源类型    
-            console.log(t);
             //获取作为判断条件的事件类型  
             var vReadOnly = obj.getAttribute('readonly');  
             var vEnabled = obj.getAttribute('enabled');  
             //处理null值情况  
             vReadOnly = (vReadOnly == null) ? false : vReadOnly;  
             vEnabled = (vEnabled == null) ? true : vEnabled;  
-            console.log(vReadOnly);
-            console.log(ev.keyCode);
             //当敲Backspace键时，事件源类型为密码或单行、多行文本的，  
             //并且readonly属性为true或enabled属性为false的，则退格键失效  
             var flag1=(ev.keyCode == 8 && (t=="password" || t=="text" || t=="textarea")   
@@ -512,7 +507,6 @@ var gkClientLogin = {
                 return false;  
             }  
             if(flag1){ 
-        
                 return false;     
             }     
         }  
