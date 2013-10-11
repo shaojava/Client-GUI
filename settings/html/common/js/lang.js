8e97ef010c6021988b516d50a61b848c4bd33019
林盛 <285319563@gokuai.com>
2013-10-11 11:45:38 +0800

    更改webview的语言获取方式

--- settings/html/common/js/lang.js
+++ settings/html/common/js/lang.js
@@ -1,5 +1,8 @@
 var lang ='zh-cn';
-if(Util.Browser.compareVersion(gkClientInterface.getClientVersion(),'2.1.5.0')>=0 &&navigator.language){
-    lang = navigator.language;
+if (Util.Browser.compareVersion(gkClientInterface.getClientVersion(), '2.1.5.0') >= 0) {
+    try {
+        lang = gkClient.gGetLanguage();
+    } catch (e) {
+    }
 }
 document.write(unescape("%3Cscript src='../common/js/lang/"+lang+".js' type='text/javascript'%3E%3C/script%3E"));
