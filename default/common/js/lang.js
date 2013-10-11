75c4f41cd353b8b9c3057f70156886f9d4f6ceb5
林盛 <285319563@gokuai.com>
2013-10-11 10:37:27 +0800

    设定默认语言值

--- default/common/js/lang.js
+++ default/common/js/lang.js
@@ -1,5 +1,8 @@
 var lang ='zh-cn';
 if(Util.Browser.compareVersion(gkClientInterface.getClientVersion(),'2.1.5.0')>=0 &&navigator.language){
     lang = navigator.language;
+    if (lang != 'zh-cn' && lang != 'en-us') {
+        lang = 'en-us';
+    }
 }
 document.write(unescape("%3Cscript src='../common/js/lang/"+lang+".js' type='text/javascript'%3E%3C/script%3E"));
